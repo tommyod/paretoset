79be5b7a2d0b5fb9d87ffa0f9ce18f23da4ac430
tommyod <tommy.odland>
2024-09-01 22:00:59 +0200

    suffix

--- docs/conf.py
+++ docs/conf.py
@@ -56,10 +56,10 @@ extensions = [
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-source_suffix = {'.md': 'restructuredtext'}
-# source_suffix = ".rst"
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = "index"
